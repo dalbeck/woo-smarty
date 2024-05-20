0afeabc5611fedb8cd75acb4f1426013ae7db501
Danny Albeck <danny.albeck@me.com>
2024-05-20 16:02:58 -0400

    updated placement of street_postdirection
    - updated version number

--- smarty-address-validation.php
+++ smarty-address-validation.php
@@ -3,7 +3,7 @@
  * Plugin Name: Smarty Address Validation for WooCommerce
  * Plugin URI: https://www.ecosmetics.com
  * Description: Integrates Smarty address validation API into WooCommerce checkout fields.
- * Version: 1.0
+ * Version: 1.0.2
  * Author: Danny Albeck
  * Author URI: https://www.ecosmetics.com
  * License: GPL2
